f1e3f195bd7dc08ddbfae039029868388c28035f
Preston Engler <pengler44@gmail.com>
2025-05-01 00:03:51 -0400

    Change background to white/transparent
    WebGpu doesn't support a transparent canvas, in that case a fallback of
    white is intended.

--- src/main.rs
+++ src/main.rs
@@ -664,7 +664,7 @@ impl State {
                 view: &output_texture_view,
                 resolve_target: None,
                 ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.0, g: 0.0, b: 0.0, a: 0.0, }),
+                    load: wgpu::LoadOp::Clear(wgpu::Color { r: 1.0, g: 1.0, b: 1.0, a: 0.0, }),
                     store: wgpu::StoreOp::Store,
                 },
             })],
